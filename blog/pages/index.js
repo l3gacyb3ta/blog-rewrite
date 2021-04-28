eb097884beb63ae631611a53aa67fd1879bdd801
Gitea <gitea@fake.local>
2021-04-27 20:52:21 -0400

    stuff.

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Footer from '../components/footer'
-import Hand   from '../components/hand'
+import Hand from '../components/hand'
 import Layout from '../components/layout'
 
 export default function Home() {
@@ -16,15 +16,18 @@ export default function Home() {
           <h1 className={styles.title}>
             <Hand></Hand>&nbsp;&nbsp;Hi, I'm Raleigh, a developer in highschool from the south.
           </h1>
-          <p className={styles.description}>
-            <a href="https://www.python.org/">Python</a>
-          </p>
-        </main>
+          
+          <div className="{styles.description}">
+              Hello! I’m Raleigh (he/they), I am a developer in high school. <br />
+              I am skilled in <strong><a href="https://www.python.org/">Python</a></strong>, <strong><a href="https://flask.palletsprojects.com/en/1.1.x/">Flask</a></strong>, <strong><a href="https://www.djangoproject.com/">Django</a></strong>,
+              <strong><a href="https://isocpp.org/">C++</a></strong>, <strong><a href="https://www.linux.org/">Linux</a></strong>, and <strong><a href="https://git-scm.com/">git</a></strong>.<br />
+              Currently I'm learning <strong><a href="https://crystal-lang.org">Crystal</a></strong>, and <strong><a href="https://nextjs.org/">Next.js</a></strong> + <strong><a href="https://reactjs.org/">React</a></strong>!
+          </div>
+        </main >
 
         <Footer>
-
         </Footer>
-      </div>
-    </Layout>
+      </div >
+    </Layout >
   )
 }
