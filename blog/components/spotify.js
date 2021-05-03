31f72433f5ecf9edf4c3de9dd6e51a91597ecc60
Gitea <gitea@fake.local>
2021-05-03 08:05:14 -0400

    Commited auto-magically because l3gacy was too lazy to actually write a commit message for this.

--- blog/components/spotify.js
+++ blog/components/spotify.js
@@ -2,10 +2,13 @@ import useSWR from 'swr'
 
 function Spotify() {
     const { data, error } = useSWR('https://spotapi.arbee.repl.co/', fetch)
-
+    
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    return <div>hello {data.name}!</div>
+    let json = data.json()
+    console.log(json)
+    return <div>: {data.title}</div>
+
 }
 
 export default Spotify;
